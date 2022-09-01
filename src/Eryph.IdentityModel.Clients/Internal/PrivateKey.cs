8a46e3288c5668bfd4174a535986e667968ed3cc
Frank Wagner <frank2568@gmail.com>
2022-09-01 12:15:12 +0200

    added keyfile protection (#16)
    added method to PrivateKey class to write the key file with ProtectedData (only working on Windows currently).

--- src/Eryph.IdentityModel.Clients/Internal/PrivateKey.cs
+++ src/Eryph.IdentityModel.Clients/Internal/PrivateKey.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security;
+using System.Security.Cryptography;
 using System.Text;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.OpenSsl;
@@ -10,8 +12,40 @@ namespace Eryph.IdentityModel.Clients.Internal
     {
         public static AsymmetricCipherKeyPair ReadFile(string filepath, IFileSystem fileSystem)
         {
-            using var reader = fileSystem.OpenText(filepath);
-            return (AsymmetricCipherKeyPair) new PemReader(reader).ReadObject();
+            using var reader = new StreamReader(fileSystem.OpenStream(filepath));
+            return (AsymmetricCipherKeyPair)new PemReader(reader).ReadObject();
+
+        }
+
+        public static AsymmetricCipherKeyPair ReadFile(string filepath, IFileSystem fileSystem, PrivateKeyProtectionLevel protectionLevel)
+        {
+            var scope = protectionLevel switch
+            {
+                PrivateKeyProtectionLevel.User => DataProtectionScope.CurrentUser,
+                PrivateKeyProtectionLevel.Machine => DataProtectionScope.LocalMachine,
+                _ => throw new ArgumentOutOfRangeException(nameof(protectionLevel), protectionLevel, null)
+            };
+
+            try
+            {
+                var entropy = Encoding.UTF8.GetBytes(filepath.ToLowerInvariant());
+                using var stream = fileSystem.OpenStream(filepath);
+
+                using var protectedDataStream = new MemoryStream();
+                stream.CopyTo(protectedDataStream);
+                protectedDataStream.Seek(0, SeekOrigin.Begin);
+
+                using var decryptedData = new MemoryStream(
+                    ProtectedData.Unprotect(protectedDataStream.GetBuffer(), entropy, scope));
+                using var reader = new StreamReader(decryptedData);
+                return (AsymmetricCipherKeyPair)new PemReader(reader).ReadObject();
+            }
+            catch (Exception)
+            {
+                //ignored
+            }
+
+            return null;
         }
 
         public static AsymmetricCipherKeyPair ReadString(string keyString)
@@ -22,8 +56,33 @@ namespace Eryph.IdentityModel.Clients.Internal
 
         public static void WriteFile(string filepath, AsymmetricCipherKeyPair keyPair, IFileSystem fileSystem)
         {
-            using var writer = fileSystem.CreateText(filepath);
+            using var writer = new StreamWriter(fileSystem.CreateStream(filepath));
             new PemWriter(writer).WriteObject(keyPair);
+        }
+
+        public static void WriteFile(string filepath, AsymmetricCipherKeyPair keyPair, IFileSystem fileSystem, PrivateKeyProtectionLevel protectionLevel)
+        {
+            var scope = protectionLevel switch
+            {
+                PrivateKeyProtectionLevel.User => DataProtectionScope.CurrentUser,
+                PrivateKeyProtectionLevel.Machine => DataProtectionScope.LocalMachine,
+                _ => throw new ArgumentOutOfRangeException(nameof(protectionLevel), protectionLevel, null)
+            };
+
+            using var memoryStream = new MemoryStream();
+            using var memoryStreamWriter = new StreamWriter(memoryStream);
+            var pem = new PemWriter(memoryStreamWriter);
+            pem.WriteObject(keyPair);
+            memoryStreamWriter.Flush();
+
+            var entropy = Encoding.UTF8.GetBytes(filepath.ToLowerInvariant());
+            
+            var protectedData = ProtectedData.Protect(
+                memoryStream.ToArray(), entropy, scope);
+
+            using var fileStream = fileSystem.CreateStream(filepath);
+            fileStream.Write(protectedData, 0, protectedData.Length);
+            fileStream.Flush();
         }
 
         public static SecureString ToSecureString(AsymmetricCipherKeyPair keyPair)
