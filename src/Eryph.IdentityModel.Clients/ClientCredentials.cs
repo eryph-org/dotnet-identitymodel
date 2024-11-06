cf44d01ef0a9f07f9b51cdad0f4a85bf52c899eb
Christopher Mann <christopher.mann@dbosoft.eu>
2024-11-06 18:49:07 +0100

    More cleanup

--- src/Eryph.IdentityModel.Clients/ClientCredentials.cs
+++ src/Eryph.IdentityModel.Clients/ClientCredentials.cs
@@ -2,67 +2,59 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
 using System.Security;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 
-namespace Eryph.IdentityModel.Clients
+namespace Eryph.IdentityModel.Clients;
+
+[PublicAPI]
+public sealed class ClientCredentials(
+    string id,
+    SecureString keyPairData,
+    Uri identityProvider,
+    string configuration)
 {
-    [PublicAPI]
-    public sealed class ClientCredentials
+    public string Id { get; } = id;
+
+    public SecureString KeyPairData { get; } = keyPairData;
+
+    public Uri IdentityProvider { get; } = identityProvider;
+
+    public string Configuration { get; } = configuration;
+
+    public Task<AccessTokenResponse> GetAccessToken(HttpClient httpClient = null)
     {
-        [DataMember]
-        public string Id { get; }
+        return GetAccessToken(null, httpClient);
+    }
 
-        public SecureString KeyPairData { get; }
+    public async Task<AccessTokenResponse> GetAccessToken(IEnumerable<string> scopes, HttpClient httpClient = null)
+    {
+        var disposeHttpClient = httpClient == null;
+        httpClient ??= new HttpClient();
 
-        public Uri IdentityProvider { get; }
+        httpClient.BaseAddress = IdentityProvider;
 
-        public string Configuration { get; }
-        
-        public ClientCredentials(string id, SecureString keyPairData, Uri identityProvider, string configuration)
+        var keyPairPtr = Marshal.SecureStringToGlobalAllocUnicode(KeyPairData);
+        try
         {
-            Id = id;
-            KeyPairData = keyPairData;
-            IdentityProvider = identityProvider;
-            Configuration = configuration;
-        }
+            var keyPair = Internal.PrivateKey.ReadString(Marshal.PtrToStringUni(keyPairPtr));
+            if (!disposeHttpClient)
+                return await httpClient.GetClientAccessToken(
+                    Id,
+                    keyPair.ToRSAParameters(), scopes).ConfigureAwait(false);
 
-        public Task<AccessTokenResponse> GetAccessToken(HttpClient httpClient = null)
-        {
-            return GetAccessToken(null, httpClient);
-        }
-
-        public async Task<AccessTokenResponse> GetAccessToken(IEnumerable<string> scopes, HttpClient httpClient = null)
-        {
-            var disposeHttpClient = httpClient == null;
-            httpClient ??= new HttpClient();
-
-            httpClient.BaseAddress = IdentityProvider;
-
-            var keyPairPtr = Marshal.SecureStringToGlobalAllocUnicode(KeyPairData);
-            try
+            using (httpClient)
             {
-                var keyPair = Internal.PrivateKey.ReadString(Marshal.PtrToStringUni(keyPairPtr));
-                if (!disposeHttpClient)
-                    return await httpClient.GetClientAccessToken(
-                        Id,
-                        keyPair.ToRSAParameters(), scopes).ConfigureAwait(false);
-
-                using (httpClient)
-                {
-                    var result = await httpClient.GetClientAccessToken(
-                        Id,
-                        keyPair.ToRSAParameters(), scopes).ConfigureAwait(false);
-                    return result;
-                }
-            }
-            finally
-            {
-                Marshal.ZeroFreeGlobalAllocUnicode(keyPairPtr);
+                var result = await httpClient.GetClientAccessToken(
+                    Id,
+                    keyPair.ToRSAParameters(), scopes).ConfigureAwait(false);
+                return result;
             }
         }
-
+        finally
+        {
+            Marshal.ZeroFreeGlobalAllocUnicode(keyPairPtr);
+        }
     }
 }
